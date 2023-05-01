5421a42602fb72faff3342efda9fbf64bad7326d
BenW2k <benworkman16@gmail.com>
2023-05-01 17:02:05 +0100

    Added BERT model and export data to csv

--- Amzn_scraper.py
+++ Amzn_scraper.py
@@ -2,12 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import pandasql as pdsql
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import numpy as np
+import re
 import secret
 from textblob import Word
 from nltk.corpus import stopwords
+import nltk
 review_list = []
 stop_words = stopwords.words('english')
 more_stop_words = ['invicta', 'would', 'one', 'got', 'two',]
+product_name = 'Invicta-8926OB-Unisex-Stainless-Automatic'
 
 # Connects to splash to render website as javascript
 def get_soup(url):
@@ -33,7 +39,7 @@ def get_reviews(soup):
 
 # Loops through each review in the set range and adds data to the review_list.
 # Contains Amazon product example
-for x in range(1,5):
+for x in range(1,10):
     soup = get_soup(f'https://www.amazon.co.uk/Invicta-8926OB-Unisex-Stainless-Automatic/product-reviews/B000JQFX1G/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber={x}')
     print(f'Getting page {x}.')
     get_reviews(soup)
@@ -41,21 +47,16 @@ for x in range(1,5):
     if not soup.find('li', {'class': 'a-disabled a-last'}):
         pass
     else:
-        break
+        break                                                                                                                            
 
 # Converts review_list into an excel file ready for data cleaning
 df = pd.DataFrame(review_list)
 # Drops any rows that have blank 'body' columns
 df.dropna(subset=['body'])
 
-def average_words(x):
-    words = x.split()
-    return sum(len(word) for word in words) / len(words)
-
 # Additional dataset features that could be useful in the future
 df['word_count'] = df['body'].apply(lambda x : len(x.split()))
 df['char_count'] = df['body'].apply(lambda x : len(x))
-df['average_word_length'] = df['body'].apply(lambda x : average_words(x))
 df['stopword_count'] = df['body'].apply(lambda x : len([word for word in x.split() if word.lower() in stop_words]))
 df['stopword_rate'] = df['stopword_count'] / df['word_count']
 
@@ -69,8 +70,25 @@ df['punc_body'] = df['lowercase_body'].str.replace('[^\w\s]', '', regex=True)
 df['no_stopword_body'] = df['punc_body'].apply(lambda x: " ".join(word for word in x.split() if word not in stop_words))
 # Removes additional stopwords found in more_stop_words from text
 df['clean_body'] = df['no_stopword_body'].apply(lambda x: " ".join(word for word in x.split() if word not in more_stop_words))
-
 # Lemmatization
 df['lemmatized'] = df['clean_body'].apply(lambda x: "" .join(Word(word).lemmatize() for word in x.split()))
 
+
+# Model building
+
+# Converts the review strings into tokens
+tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+# Instantiates the BERT model from HuggingFace
+model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+
+# Method for performing sentiment analysis using BERT sentiment model
+def sentiment_score(review):
+    tokens = tokenizer.encode(review, return_tensors='pt')
+    result = model(tokens)
+    return int(torch.argmax(result.logits)+1)
+
+#Runs sentiment analysis on 500 reviews (512 is max token limit for the model so 500 reviews is the maximum)
+df['sentiment'] = df['lemmatized'].apply(lambda x: sentiment_score(x[:100]))
+df['base sentiment'] =  df['body'].apply(lambda x: sentiment_score(x[:100]))
+df.to_csv(f'{product_name}_sentiment.csv')
 
